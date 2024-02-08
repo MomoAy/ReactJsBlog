5b8033b635a1682bc014ad6705245e571e40ddc5
MomoAy03 <mohamed.ayeva.003@gmail.com>
2024-02-08 23:21:39 +0000

    front terminer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/article" element={<Article />} />
+          <Route path="/article/:name" element={<Article />} />
           <Route path="/articleList" element={<ArticleList />} />
         </Routes>
       </div>
---
+++ client/src/Pages/Article_Content.jsx
@@ -0,0 +1,40 @@
+const articles = [
+  {
+    name: "article1",
+    title: "Les avancées récentes dans la technologie",
+    thumbnail:
+      "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: [
+      `Les récentes avancées technologiques ont transformé de nombreux aspects de notre vie quotidienne.`,
+      `Des innovations telles que l'intelligence artificielle, l'Internet des objets et la réalité virtuelle ont ouvert de nouvelles possibilités passionnantes.`,
+      `Ces technologies impactent des domaines tels que la santé, l'éducation, et même la manière dont nous interagissons avec notre environnement.`,
+      `Dans cet article, nous explorerons certaines de ces avancées et leur impact sur la société.`,
+    ],
+  },
+  {
+    name: "article2",
+    title: "L'avenir de la robotique",
+    thumbnail:
+      "https://www.wel-com.fr/wp-content/uploads/2023/05/580401-1-300x300.jpg.webp",
+    content: [
+      `La robotique est un domaine en constante évolution, et son avenir semble prometteur.`,
+      `Des robots autonomes aux exosquelettes assistés par ordinateur, les chercheurs explorent de nouvelles façons d'intégrer la technologie dans notre vie quotidienne.`,
+      `Ces développements ne se limitent pas à l'industrie, mais s'étendent également aux soins de santé, à l'exploration spatiale, et même à la surveillance environnementale.`,
+      `Dans cet article, plongeons dans les tendances actuelles de la robotique et anticipons ce que l'avenir nous réserve.`,
+    ],
+  },
+  {
+    name: "article3",
+    title: "Les défis éthiques de l'intelligence artificielle",
+    thumbnail:
+      "https://www.wel-com.fr/wp-content/uploads/2024/01/583971245-1-300x300.webp",
+    content: [
+      `L'intelligence artificielle (IA) suscite à la fois fascination et préoccupation.`,
+      `Alors que les applications de l'IA se multiplient, des questions éthiques émergent quant à l'utilisation responsable de cette technologie puissante.`,
+      `Des débats sur la confidentialité des données à la prise de décision autonome, cet article examine les défis éthiques auxquels nous sommes confrontés dans le monde de l'IA et explore les efforts visant à établir des normes éthiques pour guider son développement futur.`,
+    ],
+  },
+  // Ajoutez autant d'articles que nécessaire
+];
+
+export default articles;
